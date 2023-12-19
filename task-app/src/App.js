d7bc06240fe08cfd215cb17c09414d83a9f4ce69
Ish <nameless5660@gmail.com>
2023-12-19 05:04:05 +0000

    Revert "bug fixes"
    This reverts commit f3e6ad31ced2c471506fe231335fdfb61fd756dc.

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,53 +1,34 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './styles/AddItemPage.css'; // Make sure you have this CSS file
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import TaskList from './components/TaskList';
+import RoutineList from './components/RoutineList';
+import AddItemPage from './components/AddItemPage';
+import Tabs from './components/Tabs';
+import './App.css';
 
-function AddItemPage({ setTasks, setRoutines }) {
-  const [isAddingTask, setIsAddingTask] = useState(true);
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const [tag, setTag] = useState(''); // Only for tasks
-  const [dueDate, setDueDate] = useState(''); // Only for tasks
-  const [frequency, setFrequency] = useState(''); // Only for routines
-  const navigate = useNavigate();
+function App() {
+  const [currentView, setCurrentView] = useState('tasks'); // 'tasks' or 'routines'
+  const [tasks, setTasks] = useState([]); // Define tasks state
+  const [routines, setRoutines] = useState([]); // Define routines state
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const item = {
-      name,
-      description,
-      ...(isAddingTask ? { tag, dueDate } : { frequency }),
-    };
-
-    if (isAddingTask) {
-      setTasks(prevTasks => [...prevTasks, item]);
-    } else {
-      setRoutines(prevRoutines => [...prevRoutines, item]);
-    }
-    navigate('/');
-  };
 
   return (
-    <div className="add-item-container">
-      <div className="tabs">
-        <button className={isAddingTask ? 'active' : ''} onClick={() => setIsAddingTask(true)}>TASK</button>
-        <button className={!isAddingTask ? 'active' : ''} onClick={() => setIsAddingTask(false)}>ROUTINE</button>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-        <textarea placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
-        {isAddingTask ? (
+    <Router>
+      <Header />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={
           <>
-            <input type="text" placeholder="Tag" value={tag} onChange={(e) => setTag(e.target.value)} />
-            <input type="date" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
+            <Tabs currentView={currentView} onChangeView={setCurrentView} />
+            {currentView === 'tasks' ? <TaskList tasks={tasks} /> : <RoutineList routines={routines} />}
           </>
-        ) : (
-          <input type="text" placeholder="Frequency" value={frequency} onChange={(e) => setFrequency(e.target.value)} />
-        )}
-        <button type="submit" className="create-button">{isAddingTask ? "Create Task" : "Create Routine"}</button>
-      </form>
-    </div>
+        } />
+        <Route path="/add-item" element={<AddItemPage />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default AddItemPage;
+export default App;
