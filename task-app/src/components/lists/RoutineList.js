f3749541267e9c58ec615d528a817b7db9e8fdf1
Ish <nameless5660@gmail.com>
2024-03-15 17:04:29 +0000

    added filters
    Task and routine filters added, buttons need styling

--- task-app/src/components/lists/RoutineList.js
+++ task-app/src/components/lists/RoutineList.js
@@ -7,21 +7,13 @@ import './styles/List.css';
 function RoutineList() {
   const { currentUser } = useContext(AuthContext);
   const [routines, setRoutines] = useState([]);
+  const [filter, setFilter] = useState('all');
   const navigate = useNavigate();
-
-  // This will hold the checked state for each checkbox of every routine
-  // eslint-disable-next-line
-  const [checkboxStates, setCheckboxStates] = useState({});
 
   const fetchRoutines = useCallback(async () => {
     if (currentUser) {
       try {
         const fetchedRoutines = await getRoutinesFromDB(currentUser.uid);
-        const newCheckboxStates = {};
-        fetchedRoutines.forEach(routine => {
-          newCheckboxStates[routine.id] = routine.checkboxStates;
-        });
-        setCheckboxStates(newCheckboxStates);
         setRoutines(fetchedRoutines);
       } catch (error) {
         console.error("Error fetching routines:", error);
@@ -38,85 +30,90 @@ function RoutineList() {
     if (routine) {
         const updatedCheckboxStates = [...routine.checkboxStates];
         updatedCheckboxStates[index] = updatedCheckboxStates[index] === "unchecked" ? "checked" : "unchecked";
-
         await updateRoutineCheckboxStates(currentUser.uid, routineId, updatedCheckboxStates);
-        setCheckboxStates(prevStates => ({...prevStates, [routineId]: updatedCheckboxStates}));
-
-        const updatedRoutines = routines.map(r => r.id === routineId ? {...r, checkboxStates: updatedCheckboxStates} : r);
-        setRoutines(updatedRoutines);
+        fetchRoutines(); // Refresh routines to reflect the updated checkbox states
     }
   };
 
   const handleEditRoutine = (routineId) => {
-    navigate(`/editRoutine/${routineId}`); // Navigate to the edit routine page
+    navigate(`/editRoutine/${routineId}`);
   };
 
   const handleDeleteRoutine = async (routineId) => {
     await deleteRoutine(currentUser.uid, routineId);
-    // Refresh the task list
-    getRoutinesFromDB(currentUser.uid).then(fetchedRoutines => {
-      setRoutines(fetchedRoutines);
-    });
+    fetchRoutines(); // Refresh routines after deletion
   };
   
   const handleDeleteAllRoutines = async () => {
     for (const routine of routines) {
       await deleteRoutine(currentUser.uid, routine.id);
     }
-    // Assume all tasks are now deleted, refresh list
-    getRoutinesFromDB(currentUser.uid).then(fetchedRoutines => {
-      setRoutines(fetchedRoutines);
-    });
+    fetchRoutines(); // Refresh routines after all deletions
   };
+
+  // Filter routines based on the provided logic
+  const filteredRoutines = routines.filter(routine => {
+    const totalCheckboxes = getCheckboxCount(routine);
+    const checkedCount = routine.checkboxStates.filter(state => state === "checked").length;
+
+    switch (filter) {
+      case 'incomplete':
+        return checkedCount === 0;
+      case 'inprogress':
+        return checkedCount > 0 && checkedCount < totalCheckboxes;
+      case 'complete':
+        return checkedCount === totalCheckboxes;
+      default:
+        return true; // 'all'
+    }
+  });
 
   return (
     <div className="routine-list">
-    <div className="delete-all-container">
-    {routines.length > 0 && (
-      <button className="delete-all-button" onClick={handleDeleteAllRoutines}>Delete All Routines</button>
-    )}
-    </div>
-      {routines.length === 0 ? (
-        <div className="empty-list">You have no Routines</div>
+      <div className="options">
+        <div className="filter-buttons">
+          <button onClick={() => setFilter('all')}>All Routines</button>
+          <button onClick={() => setFilter('incomplete')}>Incomplete Routines</button>
+          <button onClick={() => setFilter('inprogress')}>In Progress Routines</button>
+          <button onClick={() => setFilter('complete')}>Complete Routines</button>
+        </div>
+        <div className="delete-all-container">
+          {routines.length > 0 && (
+            <button className="delete-all-button" onClick={handleDeleteAllRoutines}>Delete All Routines</button>
+          )}
+        </div>
+      </div>
+      {filteredRoutines.length === 0 ? (
+        <div className="empty-list">No routines match this filter</div>
       ) : (
-        routines.map((routine, index) => {
-          const isCheckedCount = routine.checkboxStates.filter(state => state === "checked").length;
-          const totalCheckboxes = getCheckboxCount(routine);
-          const progressPercentage = (isCheckedCount / totalCheckboxes) * 100;
-
-          return (
-            <div key={index} className="list-item">
-              <div className="list-item-inner">
-                <div className="item-content">
-                  <div className="item-detail">
-                    <h3 className="item-name">{routine.name}</h3>
-                    <h4 className="item-description">{routine.description}</h4>
-                    <p className="item-frequency">Frequency: {routine.frequencyNumber} per {routine.frequencyPeriod}</p>
-                    <div className="checkboxes-container" 
-                      style={{background: `linear-gradient(to right, #4CAF50 
-                      ${progressPercentage}%, #ff4081 
-                      ${progressPercentage}%, #ff4081 100%)`
-                    }}>
-                      {routine.checkboxStates.map((state, i) => (
-                        <input
-                          key={i}
-                          type="checkbox"
-                          className="routine-checkbox"
-                          checked={state === "checked"}
-                          onChange={() => handleCheckboxChange(routine.id, i)}
-                        />
-                      ))}
-                    </div>
-                    <div>
+        filteredRoutines.map((routine, index) => (
+          <div key={index} className="list-item">
+            <div className="list-item-inner">
+              <div className="item-content">
+                <div className="item-detail">
+                  <h3 className="item-name">{routine.name}</h3>
+                  <h4 className="item-description">{routine.description}</h4>
+                  <p className="item-frequency">Frequency: {routine.frequencyNumber} per {routine.frequencyPeriod}</p>
+                  <div className="checkboxes-container">
+                    {routine.checkboxStates.map((state, i) => (
+                      <input
+                        key={i}
+                        type="checkbox"
+                        className="routine-checkbox"
+                        checked={state === "checked"}
+                        onChange={() => handleCheckboxChange(routine.id, i)}
+                      />
+                    ))}
+                  </div>
+                  <div className="button-container">
                     <button className="edit-button" onClick={() => handleEditRoutine(routine.id)}>Edit</button>
                     <button className="delete-button" onClick={() => handleDeleteRoutine(routine.id)}>Delete</button>
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          );
-        })
+          </div>
+        ))
       )}
     </div>
   );
